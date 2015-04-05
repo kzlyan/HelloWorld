58cf9e41d4b09414f0b658e2bdeb1ac8fb99d3fb
kzlyan <kzlyan@tut.by>
2015-04-05 14:14:35 +0300

    first

---
+++ ConsoleApplicationGitVS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationGitVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello TFS and Git");
+        }
+    }
+}
